2a1f04cfe04ff9107baebd4cde1b5af09d900333
mhaut <juanmariohaut@unex.es>
2016-07-14 15:58:59 +0200

    fix erros hang bad manage threads

--- clientSim.py
+++ clientSim.py
@@ -9,10 +9,10 @@ from PyQt4 import QtCore
 
 
 class FileThread(QtCore.QThread):
-	def __init__(self,pathFiles=None,fileName=None):
+	def __init__(self,pathFiles=None,fileName=None,ip="localhost"):
 		QtCore.QThread.__init__(self, None)
 		self.jsonImageSim = '{"md5":"1307e461d318b42fce286ba3e9cf9f43","metadata":{"X Resolution":"72 dots per inch","Y Resolution":"72 dots per inch","Resolution Unit":"Inch","Model":"Nexus 5","Date/Time":"2016:04:16 01:08:23","Image Height":"2448 pixels","Image Width":"3264 pixels","File Name":"JPEG_20160416_010818_1525679884.jpg","File Size":"1844706 bytes",},"location":{"lat":"39.45921064","lng":"-6.38312189"},"tags":[{"name":"cojin"},{"name":"salon"}]}'
-		self.ipDirection = "http://localhost:12342"
+		self.ipDirection = "http://92.222.90.158:12342"
 		self.fileUpload = pathFiles+fileName
 
 	def setProcessImageCounter(self, counter):
@@ -33,16 +33,17 @@ class FileThread(QtCore.QThread):
 		req.add_header('Content-Type', 'application/json')
 		response = urllib2.urlopen(req, json.dumps(ast.literal_eval(self.jsonImageSim)))
 		print "ENVIADO"
-		self.terminate()
+		#self.terminate()
 
 
 if __name__ == '__main__':
-	filesInPackage = 1
+	filesInPackage = 100
+	ip             = "localhost"
 	jsonPkgSim   = '{"session": {"date": "01072016,19:16","model": "Nexus5","location": {"lat": "39.4783691","lng": "-6.3421245"},"id": "1467393396324","name": "nombredelpaquete"}}'
 	allThreads = [None for i in range(filesInPackage)]
 
 	for i in range(filesInPackage):
-		allThreads[i] = FileThread(pathFiles="gatitos/",fileName="gatitos"+str(i)+".jpg")
+		allThreads[i] = FileThread(pathFiles="gatitos/",fileName="gatitos"+str(i)+".jpg",ip=ip)
 
 	for i in range(filesInPackage):
 		if allThreads[i].isRunning() == False:
@@ -53,10 +54,11 @@ if __name__ == '__main__':
 		running = 0
 		for i in range(len(allThreads)):
 			running += allThreads[i].isRunning()
-		if running == 0: # 
+		if running == 0: # not threads running
 			break
 
-	req = urllib2.Request("http://localhost:12342/json")
+
+	req = urllib2.Request("http://"+ip+":12342/json")
 	req.add_header('Content-Type', 'application/json')
 	response = urllib2.urlopen(req, json.dumps(ast.literal_eval(jsonPkgSim)))
 	print "TODO SUBIDO"
